06cfc6fbba831ec299d8cb661e1c760b150987a3
Loc Nguyen <ntploc21@gmail.com>
2023-04-07 00:30:40 +0700

    [Update] Change dynamic array title

---
+++ src/Identifiers/CategoryIdentifiers.hpp
@@ -0,0 +1,8 @@
+#ifndef CATEGORYIDENTIFIERS_HPP
+#define CATEGORYIDENTIFIERS_HPP
+
+namespace Category {
+    enum ID { None, Array, LinkedList, Count };
+};
+
+#endif  // CATEGORYIDENTIFIERS_HPP
---
+++ src/Identifiers/DSIdentifiers.hpp
@@ -0,0 +1,18 @@
+#ifndef DSIDENTIFIERS_HPP
+#define DSIDENTIFIERS_HPP
+
+namespace DataStructures {
+    enum ID {
+        None,
+        StaticArray,
+        DynamicArray,
+        SinglyLinkedList,
+        DoublyLinkedList,
+        CircularLinkedList,
+        Stack,
+        Queue,
+        Count
+    };
+};
+
+#endif  // DSIDENTIFIERS_HPP
