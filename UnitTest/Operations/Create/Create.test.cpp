c8cb05a9b81a4dade94a60b54cce3d07f959571c
ntploc21 <ntploc21@gmail.com>
2023-04-10 15:16:38 +0700

    [Add] Add BaseNode for LList and SLL data structure

--- UnitTest/Operations/Create/Create.test.cpp
+++ UnitTest/Operations/Create/Create.test.cpp
@@ -1,25 +1,24 @@
-// #include "../../../src/Core/Operations/Create/Create.hpp"
+#include "../../../src/Core/Operations/Create/Create.hpp"
 
 #include "doctest.h"
 
 TEST_CASE("Base Create functionality") {
-    // Create createOp = Create();
-    // SUBCASE("Testing Create::RandomFixedSize") {
-    //     std::map< std::string, std::string > params;
-    //     params["nSize"] = "5";
-    //     std::vector< int > output =
-    //         createOp.CreateInput(CreateOperation::RandomFixedSize, params);
+    Create createOp = Create();
+    SUBCASE("Testing Create::RandomFixedSize") {
+        std::map< std::string, std::string > params;
+        params["nSize"] = "5";
+        std::vector< int > output =
+            createOp.CreateInput(CreateOperation::RandomFixedSize, params);
 
-    //     CHECK(output.size() == 5);
-    // }
+        CHECK(output.size() == 5);
+    }
 
-    // SUBCASE("Testing Create::UserDefined") {
-    //     std::map< std::string, std::string > params;
-    //     params["input"] = "5,10,3,5,9";
-    //     std::vector< int > output =
-    //         createOp.CreateInput(CreateOperation::UserDefined, params);
+    SUBCASE("Testing Create::UserDefined") {
+        std::map< std::string, std::string > params;
+        params["input"] = "5,10,3,5,9";
+        std::vector< int > output =
+            createOp.CreateInput(CreateOperation::UserDefined, params);
 
-    //     CHECK(output.size() == 5);
-    // }
-    CHECK(1 == 1);
+        CHECK(output.size() == 5);
+    }
 }
