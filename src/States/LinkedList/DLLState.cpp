ad1c5eb46d59d623218305c3d3525ae27ad60e40
ntploc21 <ntploc21@gmail.com>
2023-04-14 00:05:47 +0700

    [Update] Update Doubly Linked List screen actions

--- src/States/LinkedList/DLLState.cpp
+++ src/States/LinkedList/DLLState.cpp
@@ -1,10 +1,197 @@
 #include "DLLState.hpp"
 
+#include <iostream>
+
+#include "../../Global.hpp"
+
 DLLState::DLLState(StateStack& stack, Context context)
-    : LLState(stack, context, DataStructures::DoublyLinkedList) {}
+    : LLState(stack, context, DataStructures::DoublyLinkedList) {
+    AddOperations();
+}
 
 DLLState::~DLLState() {}
 
-void DLLState::Draw() { navigation.Draw(); }
+void DLLState::Draw() {
+    DrawRectangle(0, global::SCREEN_HEIGHT - 40, global::SCREEN_WIDTH, 40,
+                  BLACK);
+    DrawRectangle(0, 0, 40, global::SCREEN_HEIGHT, BLACK);
+
+    DrawRectangle(global::SCREEN_WIDTH - 40, 0, 40, global::SCREEN_HEIGHT,
+                  BLACK);
+
+    operationList.Draw();
+    navigation.Draw();
+}
 
 bool DLLState::Update(float dt) { return true; }
+
+void DLLState::AddInsertOperation() {
+    Button::Ptr buttonInsert(new Button("Insert", GetContext().fonts));
+    OperationContainer::Ptr container(new OperationContainer());
+
+    /* ==== DEFINE OPERATIONS FOR INSERT ==== */
+
+    /* Insert head */
+    AddIntFieldOperationOption(
+        container, "i = 0 (Head), specify v =", {{"v = ", 50, 0, 99}},
+        [this](std::map< std::string, std::string > input) {
+            std::cout << "i = 0 (Head), specify v =" << std::endl;
+            for (auto it : input) {
+                std::cout << it.first << it.second << std::endl;
+            }
+        });
+
+    /* Insert tail */
+    AddIntFieldOperationOption(
+        container, "i = N (After Tail), specify v =", {{"v = ", 50, 0, 99}},
+        [this](std::map< std::string, std::string > input) {
+            std::cout << "i = N (After Tail), specify v =" << std::endl;
+            for (auto it : input) {
+                std::cout << it.first << it.second << std::endl;
+            }
+        });
+
+    /* Default insert */
+
+    AddIntFieldOperationOption(
+        container, "Specify both i in [0..N] and v",
+        {{"i = ", 50, 0, 9}, {"v = ", 50, 0, 99}},
+        [this](std::map< std::string, std::string > input) {
+            std::cout << "Specify both i in [0..N] and v parameters: "
+                      << std::endl;
+            for (auto it : input) {
+                std::cout << it.first << it.second << std::endl;
+            }
+        });
+
+    /* ====================================== */
+    operationList.AddOperation(buttonInsert, container);
+}
+
+void DLLState::AddInitializeOperation() {
+    Button::Ptr buttonInitialize(new Button("Create", GetContext().fonts));
+    OperationContainer::Ptr container(new OperationContainer());
+
+    /* ==== DEFINE OPERATIONS FOR CREATE ==== */
+
+    /* Empty */
+    AddNoFieldOperationOption(container, "Empty",
+                              [this]() { std::cout << "Empty" << std::endl; });
+
+    /* Random */
+
+    AddNoFieldOperationOption(container, "Random",
+                              [this]() { std::cout << "Random" << std::endl; });
+
+    /* Random Sorted */
+    AddNoFieldOperationOption(container, "Random Sorted", [this]() {
+        std::cout << "Random Sorted" << std::endl;
+    });
+
+    /* Random Fixed Size */
+    AddIntFieldOperationOption(
+        container, "Random Fixed Size", {{"i = ", 50, 0, 9}},
+        [this](std::map< std::string, std::string > input) {
+            std::cout << "Random Fixed Size parameters:" << std::endl;
+
+            for (auto it : input) {
+                std::cout << it.first << it.second << std::endl;
+            }
+        });
+
+    /* User defined */
+    AddStringFieldOption(
+        container, "--- User defined list ---",
+        "arr = ", [this](std::map< std::string, std::string > input) {
+            std::cout << "--- User defined list --- parameters:" << std::endl;
+
+            for (auto it : input) {
+                std::cout << it.first << it.second << std::endl;
+            }
+        });
+
+    /* ====================================== */
+    operationList.AddOperation(buttonInitialize, container);
+}
+
+void DLLState::AddUpdateOperation() {
+    Button::Ptr buttonUpdate(new Button("Update", GetContext().fonts));
+    /*  */
+
+    OperationContainer::Ptr container(new OperationContainer());
+
+    /* ==== DEFINE OPERATIONS FOR UPDATE ==== */
+
+    AddIntFieldOperationOption(
+        container, "Specify i in [0..N-1] and v",
+        {{"i = ", 50, 0, 9}, {"v = ", 50, 0, 99}},
+        [this](std::map< std::string, std::string > input) {
+            std::cout << "Specify i in [0..N-1] and v parameters: "
+                      << std::endl;
+            for (auto it : input) {
+                std::cout << it.first << it.second << std::endl;
+            }
+        });
+
+    operationList.AddOperation(buttonUpdate, container);
+}
+
+void DLLState::AddDeleteOperation() {
+    Button::Ptr buttonDelete(new Button("Delete", GetContext().fonts));
+    OperationContainer::Ptr container(new OperationContainer());
+
+    /* ==== DEFINE OPERATIONS FOR DELETE ==== */
+
+    /* Delete head */
+
+    AddNoFieldOperationOption(container, "i = 0 (Head)", [this]() {
+        std::cout << "i = 0 (Head)" << std::endl;
+    });
+
+    /* Delete tail */
+    AddNoFieldOperationOption(container, "i = N-1 (Tail)", [this]() {
+        std::cout << "i = N-1 (Tail)" << std::endl;
+    });
+
+    /* Delete specific element */
+
+    AddIntFieldOperationOption(
+        container, "Specify i in [1..N-1]", {{"i = ", 50, 0, 9}},
+        [this](std::map< std::string, std::string > input) {
+            std::cout << "Specify i in [1..N-1]" << std::endl;
+            for (auto it : input) {
+                std::cout << it.first << it.second << std::endl;
+            }
+        });
+    /* Delete elements with specific value */
+
+    AddIntFieldOperationOption(
+        container, "Specify v", {{"v = ", 50, 0, 99}},
+        [this](std::map< std::string, std::string > input) {
+            std::cout << "Specify v" << std::endl;
+            for (auto it : input) {
+                std::cout << it.first << it.second << std::endl;
+            }
+        });
+    operationList.AddOperation(buttonDelete, container);
+}
+
+void DLLState::AddSearchOperation() {
+    Button::Ptr buttonSearch(new Button("Search", GetContext().fonts));
+    OperationContainer::Ptr container(new OperationContainer());
+
+    /* ==== DEFINE OPERATIONS FOR SEARCH ==== */
+
+    /* Search for the first element that has value of v */
+
+    AddIntFieldOperationOption(
+        container, "Specify v", {{"v = ", 50, 0, 99}},
+        [this](std::map< std::string, std::string > input) {
+            std::cout << "Specify v" << std::endl;
+            for (auto it : input) {
+                std::cout << it.first << it.second << std::endl;
+            }
+        });
+
+    operationList.AddOperation(buttonSearch, container);
+}
