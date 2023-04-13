f7a4f7f578c1fba40d889603d51b24ad0b3f476b
ntploc21 <ntploc21@gmail.com>
2023-04-14 02:00:07 +0700

    [Update] Added actions for base Array screne

--- src/States/Array/ArrayState.cpp
+++ src/States/Array/ArrayState.cpp
@@ -1,9 +1,16 @@
 #include "ArrayState.hpp"
 
+#include <iostream>
+
+#include "../../Components/IntegerInputField.hpp"
+#include "../../Components/StringInputField.hpp"
+#include "../../Global.hpp"
+
 ArrayState::ArrayState(StateStack& stack, Context context,
                        DataStructures::ID activeDS)
     : State(stack, context), activeDS(activeDS) {
     InitNavigationBar();
+    operationList = OperationList(context.fonts);
 }
 
 ArrayState::~ArrayState() {}
@@ -17,4 +24,67 @@ void ArrayState::InitNavigationBar() {
         auto dsInfo = GetContext().dsInfo->Get(dsID);
         navigation.InsertTitle(dsID, dsInfo.stateID, dsInfo.abbr, dsInfo.name);
     };
+}
+
+void ArrayState::AddOperations() {
+    AddInitializeOperation();
+    AddInsertOperation();
+    AddDeleteOperation();
+    AddUpdateOperation();
+    AddSearchOperation();
+
+    operationList.SetPosition(
+        0, global::SCREEN_HEIGHT - 60 - operationList.GetSize().y);
+    operationList.InitActionBar();
+}
+
+void ArrayState::AddInitializeOperation() {}
+
+void ArrayState::AddInsertOperation() {}
+
+void ArrayState::AddDeleteOperation() {}
+
+void ArrayState::AddUpdateOperation() {}
+
+void ArrayState::AddSearchOperation() {}
+
+void ArrayState::AddNoFieldOperationOption(OperationContainer::Ptr container,
+                                           std::string title,
+                                           std::function< void() > action) {
+    OptionInputField::Ptr button(new OptionInputField(GetContext().fonts));
+
+    button.get()->SetNoFieldOption(title, action);
+
+    container.get()->pack(button);
+}
+
+void ArrayState::AddIntFieldOperationOption(
+    OperationContainer::Ptr container, std::string title,
+    std::vector< IntegerInput > fields,
+    std::function< void(std::map< std::string, std::string >) > action) {
+    OptionInputField::Ptr button(new OptionInputField(GetContext().fonts));
+    std::vector< InputField::Ptr > intFields;
+    for (auto field : fields) {
+        IntegerInputField::Ptr intField(
+            new IntegerInputField(GetContext().fonts));
+        intField.get()->SetLabel(field.label);
+        intField.get()->SetInputFieldSize((Vector2){(float)field.width, 30});
+        intField.get()->SetConstraint(field.minValue, field.maxValue);
+        intFields.push_back(intField);
+    }
+
+    button.get()->SetOption(title, intFields, action);
+
+    container.get()->pack(button);
+}
+
+void ArrayState::AddStringFieldOption(
+    OperationContainer::Ptr container, std::string title, std::string label,
+    std::function< void(std::map< std::string, std::string >) > action) {
+    OptionInputField::Ptr button(new OptionInputField(GetContext().fonts));
+    StringInputField::Ptr strField(new StringInputField(GetContext().fonts));
+    strField.get()->SetLabel(label);
+    strField.get()->SetInputFieldSize((Vector2){100, 30});
+    button.get()->SetOption(title, {strField}, action);
+    container.get()->pack(button);
 }
--- src/States/Array/ArrayState.hpp
+++ src/States/Array/ArrayState.hpp
@@ -1,10 +1,20 @@
-#ifndef STATES_ARRAY_ARRAYSTATE_HPP
-#define STATES_ARRAY_ARRAYSTATE_HPP
+#ifndef STATES_LINKEDLIST_ARRAYSTATE_HPP
+#define STATES_LINKEDLIST_ARRAYSTATE_HPP
 
+#include "../../Components/OperationContainer.hpp"
+#include "../../Components/OperationList.hpp"
+#include "../../Components/OptionInputField.hpp"
 #include "../../State.hpp"
 
 class ArrayState : public State {
-private:
+public:
+    struct IntegerInput {
+        std::string label;
+        int width;
+        int minValue;
+        int maxValue;
+    };
+
 public:
     ArrayState(StateStack& stack, Context context, DataStructures::ID activeDS);
     ~ArrayState();
@@ -15,8 +25,29 @@ protected:
     void InitNavigationBar();
     Context mContext;
 
+protected:
+    OperationList operationList;
+    virtual void AddOperations();  // DO NOT OVERRIDE THIS FUNCTION
+    virtual void AddInitializeOperation();
+    virtual void AddInsertOperation();
+    virtual void AddDeleteOperation();
+    virtual void AddUpdateOperation();
+    virtual void AddSearchOperation();
+
+protected:
+    virtual void AddNoFieldOperationOption(OperationContainer::Ptr container,
+                                           std::string title,
+                                           std::function< void() > action);
+    virtual void AddIntFieldOperationOption(
+        OperationContainer::Ptr container, std::string title,
+        std::vector< IntegerInput > fields,
+        std::function< void(std::map< std::string, std::string >) > action);
+    virtual void AddStringFieldOption(
+        OperationContainer::Ptr container, std::string title, std::string label,
+        std::function< void(std::map< std::string, std::string >) > action);
+
 private:
     DataStructures::ID activeDS;
 };
 
-#endif  // STATES_ARRAY_ARRAYSTATE_HPP
+#endif  // STATES_LINKEDLIST_ARRAYSTATE_HPP
