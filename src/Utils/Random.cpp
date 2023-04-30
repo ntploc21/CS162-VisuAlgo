b2f0376107ba13e0c8ae41dc7cedb41a068cb3a7
ntploc21 <ntploc21@gmail.com>
2023-04-30 16:29:14 +0700

    [Update] Merge Random util functions -> Utils

--- src/Utils/Random.cpp
+++
@@ -1,8 +0,0 @@
-#include "Random.hpp"
-
-int Random::Rand(int lower, int upper) {
-    std::mt19937 rng(
-        std::chrono::steady_clock::now().time_since_epoch().count());
-    if (lower > upper) std::__throw_domain_error("Invalid range");
-    return lower + rng() % (upper - lower + 1);
-}
--- src/Utils/Utils.cpp
+++ src/Utils/Utils.cpp
@@ -2,6 +2,13 @@
 
 #include "raygui.h"
 
+int Utils::Rand(int lower, int upper) {
+    std::mt19937 rng(
+        std::chrono::steady_clock::now().time_since_epoch().count());
+    if (lower > upper) std::__throw_domain_error("Invalid range");
+    return lower + rng() % (upper - lower + 1);
+}
+
 // Draw text using font inside rectangle limits
 void Utils::DrawTextBoxed(Font font, const char *text, Rectangle rec,
                           float fontSize, float spacing, bool wordWrap,
