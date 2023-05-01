b370b1429296cce9828e3a0129bc1c86f6f198ae
ntploc21 <ntploc21@gmail.com>
2023-05-01 20:23:43 +0700

    [Update] Made List<T> as base class for implementation of every List class (Deque, Stack, and Queue), and add Core::Deque unit tests

--- src/Core/List.hpp
+++ src/Core/List.hpp
@@ -48,6 +48,39 @@ namespace Core {
             ++this->mSize;
         }
 
+        /* @brief Move nodes from a list of range [`first`, `last`) to before
+         * `it` */
+        /* @brief Return iterator pointing to the first inserted element, or
+         * `it` if `first` == `last`. */
+        /* @exception: undefined behaviour: null pointer reference */
+        iterator move_previous(const iterator& it, const iterator& first,
+                               const iterator& last) {
+            mSize += std::distance(first, last);
+
+            iterator prev = it;
+            prev--;
+
+            if (first != last) {
+                Node< T >* node = it.ptr;
+                Node< T >* node_first = first.ptr;
+                Node< T >* node_last = last.ptr->mPrev;
+
+                node_first->mPrev = node->mPrev;
+                if (node->mPrev != nullptr) {
+                    node->mPrev->mNext = node_first;
+                }
+
+                node_last->mNext = node;
+                node->mPrev = node_last;
+
+                if (it == this->begin()) {
+                    this->mBegin = node_first;
+                }
+            }
+
+            return prev == nullptr ? this->begin() : prev++;
+        }
+
         iterator get_iterator(std::size_t index) {
             if (!(index >= 0 && index < this->size())) {
                 throw std::out_of_range("Index out of range");
@@ -59,7 +92,7 @@ namespace Core {
                 for (std::size_t i = 0; i < index; ++i) ++it;
             } else {
                 it = this->end();
-                for (std::size_t i = this->size() - 1; i > index; --i) --it;
+                for (std::size_t i = this->size() - 1; i >= index; --i) --it;
             }
             return it;
         }
@@ -73,6 +106,7 @@ namespace Core {
             }
         }
 
+        /* @brief Copy constructor */
         List(const List< T >& list) : mSize(0) {
             this->mBegin = this->mEnd = new Node< T >();
             for (const auto& element : list) {
@@ -80,17 +114,41 @@ namespace Core {
             }
         }
 
-    public:
+        /* @brief Move constructor */
+        List(List< T >&& list) : mSize(0) {
+            this->mBegin = this->mEnd = new Node< T >();
+            move_previous(this->end(), list.begin(), list.end());
+            list.reset();
+        }
+
+        List< T >& operator=(std::initializer_list< T > list) {
+            assign(list);
+            return *this;
+        }
+        List< T >& operator=(const List< T >& list) {
+            if (this == &list) return *this;
+            this->assign(list.begin(), list.end());
+            return *this;
+        }
+        List< T >& operator=(List< T >&& list) {
+            if (this == &list) return *this;
+            this->clear();
+            this->move_previous(this->end(), list.begin(), list.end());
+            list.reset();
+            return *this;
+        }
+
+    protected:
         iterator begin() { return mBegin; }
         const_iterator begin() const { return mBegin; }
         iterator end() { return mEnd; }
         const_iterator end() const { return mEnd; }
 
     public:
-        bool empty() const { return this->size() == 0; }
-        std::size_t size() const { return this->mSize; }
+        [[nodiscard]] bool empty() const { return this->size() == 0; }
+        [[nodiscard]] std::size_t size() const { return this->mSize; }
 
-    public:
+    protected:
         /* Exception(s): undefined behavior: null pointer dereference */
         T& front() { return *this->begin(); }
         const T& front() const { return *this->begin(); }
@@ -135,6 +193,7 @@ namespace Core {
             this->remove(it);
         }
 
+    protected:
         iterator remove(const iterator& it) {
             if (it == this->end()) {
                 throw std::out_of_range("Iterator out of range");
@@ -170,11 +229,16 @@ namespace Core {
             Return resulting size
         */
         int remove(const T& value) {
-            return this->remove_if(value, this->begin(), this->end());
+            return this->remove_if(
+                [&value](const T& element) { return element == value; },
+                this->begin(), this->end());
         }
 
         /*
-            Return resulting size
+            @brief Removes all elements satisfying specific criteria from the
+           range [first, last) and returns a past-the-end iterator for the new
+           end of the range.
+            @return the resulting size
         */
         int remove_if(std::function< bool(const T&) > predicate,
                       const iterator& begin, const iterator& end) {
@@ -189,10 +253,115 @@ namespace Core {
         }
 
         /*
-            Return resulting size
+            @brief Removes all elements satisfying specific criteria from the
+           range [first, last) and returns a past-the-end iterator for the new
+           end of the range.
+            @return the resulting size
         */
         int remove_if(std::function< bool(const T&) > predicate) {
             return this->remove_if(predicate, this->begin(), this->end());
+        }
+
+        void resize(std::size_t count) {
+            while (this->size() < count) this->push_back(T());
+
+            while (this->size() > count) this->pop_back();
+        }
+
+        void resize(std::size_t count, const T& value) {
+            while (this->size() < count) this->push_back(value);
+
+            while (this->size() > count) this->pop_back();
+        }
+
+        void assign(std::size_t count, const T& value) {
+            this->clear();
+            this->resize(count, value);
+        }
+
+        /*
+            @brief Assigns new contents to the list, replacing its current
+           content with [`first`, `last`)
+            @note [`first`, `last`) must not be part of this list, and `last`
+           must be reachable by `first`
+        */
+        void assign(const const_iterator& begin, const const_iterator& end) {
+            this->resize(std::distance(begin, end));
+
+            auto itOther = begin;
+            for (auto it = this->begin(); it != this->end(); ++it, ++itOther) {
+                *it = *itOther;
+            }
+        }
+
+        /*
+            @brief Assigns new contents to the list, replacing its current
+           content
+        */
+        void assign(std::initializer_list< T > list) {
+            this->resize(list.size());
+
+            auto itOther = list.begin();
+            for (auto it = this->begin(); it != this->end(); ++it, ++itOther) {
+                (*it) = (*itOther);
+            }
+        }
+
+        /*
+            @brief Eliminates all except the first element from every
+           consecutive group of equivalent elements from the range [`first`,
+           `last`) and returns a past-the-end iterator for the new logical end
+           of the range.
+            @return the resulting size
+        */
+        std::size_t unique() {
+            iterator last = std::unique(this->begin(), this->end());
+            this->resize(std::distance(this->begin(), last));
+
+            return this->size();
+        }
+
+        std::size_t unique(
+            std::function< bool(const T&, const T&) > predicate) {
+            iterator last = std::unique(this->begin(), this->end(), predicate);
+            this->resize(std::distance(this->begin(), last));
+
+            return this->size();
+        }
+
+        /*
+            @brief Reverses the order of the elements in the list
+        */
+        void reverse() {
+            if (!this->empty()) {
+                for (auto it = this->begin(), itNext = this->begin();
+                     it != this->end(); it = itNext) {
+                    itNext = it;
+                    ++itNext;
+
+                    Node< T >* node = it.ptr;
+                    Node< T >* node_next = itNext.ptr;
+
+                    node->mNext = node->mPrev;
+                    node->mPrev = node_next;
+                }
+            }
+
+            iterator new_head = this->end();
+            --new_head;
+            iterator new_tail = this->begin();
+
+            new_head.ptr->mPrev = nullptr;
+            new_tail.ptr->mNext = this->end().ptr;
+            this->mBegin = new_head;
+            this->mEnd.ptr->mPrev = new_tail.ptr;
+        }
+
+    protected:
+        void swap(List< T >& other) {
+            std::swap(this->mBegin, other.mBegin);
+            std::swap(this->mEnd, other.mEnd);
+            std::swap(this->mSize, other.mSize);
         }
 
     public:
@@ -310,7 +479,7 @@ namespace Core {
         const_iterator(const iterator& other) : ptr(other.ptr) {}
         const_iterator(const const_iterator& other) : ptr(other.ptr) {}
         reference operator*() const {
-            reference value = ptr->value;
+            reference value = ptr->mValue;
             return value;
         }
 
@@ -318,7 +487,7 @@ namespace Core {
 
         /* Prefix increment */
         const_iterator& operator++() {
-            ptr = ptr->next;
+            ptr = ptr->mNext;
             return *this;
         }
 
