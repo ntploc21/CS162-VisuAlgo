bce5db35c63889b83d965dab6065413b27d1884f
ntploc21 <ntploc21@gmail.com>
2023-05-01 20:17:38 +0700

    [Fix] Minor fix Utils::Random(lower, upper) (swap lower and upper when lower > upper)

--- src/Utils/Utils.cpp
+++ src/Utils/Utils.cpp
@@ -32,7 +32,7 @@ std::string Utils::ReadInputFromFile(std::string path) {
 int Utils::Rand(int lower, int upper) {
     std::mt19937 rng(
         std::chrono::steady_clock::now().time_since_epoch().count());
-    if (lower > upper) std::__throw_domain_error("Invalid range");
+    if (lower > upper) std::swap(lower, upper);
     return lower + rng() % (upper - lower + 1);
 }
 
